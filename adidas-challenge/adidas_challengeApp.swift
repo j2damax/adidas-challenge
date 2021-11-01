d5b34f55ac1405949e54bcfa405f901d3a2059b8
Jayampathy Balasuriya <jayampathy@gapstars.net>
2021-11-01 08:53:32 +0530

    adding initial project

---
+++ adidas-challenge/adidas_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  adidas_challengeApp.swift
+//  adidas-challenge
+//
+//  Created by Jayampathy Balasuriya on 2021-11-01.
+//
+
+import SwiftUI
+
+@main
+struct adidas_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
